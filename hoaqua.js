a1adde7839d31ec819bfd02e9e16b6fe4e50d0c5
Phi Hung <phihunds1@gmail.com>
2020-10-23 20:04:29 +0700

    them oi va dua hau

---
+++ hoaqua.js
@@ -0,0 +1,9 @@
+function tracuu() {
+    let tenqua = document.getElementById("tenqua").value
+    switch (tenqua) {
+        case "Ổi":
+            document.getElementById("hienthi").innerText = "20.000 VNĐ/kg"
+        break
+        case "Dưa Hấu"
+    }
+}
