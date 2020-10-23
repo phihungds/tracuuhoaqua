896ea54361cf54580888a6d0abbc2828aa59b784
Phi Hung <phihunds1@gmail.com>
2020-10-23 20:10:42 +0700

    hoanthien

--- hoaqua.js
+++ hoaqua.js
@@ -2,8 +2,21 @@ function tracuu() {
     let tenqua = document.getElementById("tenqua").value
     switch (tenqua) {
         case "Ổi":
+        case "Dưa Hấu":
             document.getElementById("hienthi").innerText = "20.000 VNĐ/kg"
         break
-        case "Dưa Hấu"
+        case "Táo":
+        case "Xoài":
+            document.getElementById("hienthi").innerText = "30.000 VNĐ/kg"
+        break
+        case "Cam":
+        case "Chôm Chôm":
+            document.getElementById("hienthi").innerText = "40.000 VNĐ/kg"
+        break
+        case "Măng Cụt":
+            document.getElementById("hienthi").innerText = "50.000 VNĐ/kg"
+        break
+        default :
+            document.getElementById("hienthi").innerText = "Không tìm thấy quả này"
     }
 }
